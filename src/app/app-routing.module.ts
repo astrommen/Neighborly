3a790ff0135c6b2b711d3bbbe83e038ef54cb568
Aja Strommen <a.g.mechanix@gmail.com>
2020-12-25 00:45:58 -0600

    created search results page with working route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { SearchResultsComponent } from './components/pages/search-results/search-results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'results', component: SearchResultsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
-
+import { SearchResultsComponent } from './components/pages/search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-
+    SearchResultsComponent,
   ],
   imports: [
     BrowserModule,
