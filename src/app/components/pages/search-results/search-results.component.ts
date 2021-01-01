76a8502810e7769a3f1aa5fa122f9b687f8df0f8
Aja Strommen <a.g.mechanix@gmail.com>
2021-01-01 01:48:25 -0600

    refactored results to results-list comp, show list on init of results pg

--- src/app/components/pages/search-results/search-results.component.ts
+++ src/app/components/pages/search-results/search-results.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchResult } from 'src/app/models/SearchResult';
-
 
 @Component({
   selector: 'app-search-results',
@@ -12,8 +10,6 @@ export class SearchResultsComponent implements OnInit {
   displayList: boolean;
   displayMap: boolean;
 
-  results:SearchResult[];
-
   showHideComp(value) {
     this.displayList = value;
     this.displayMap = !value;
@@ -23,26 +19,8 @@ export class SearchResultsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.results = [
-      {
-        id: 1,
-        // business_status: "OPERATIONAL",
-        formatted_address: "3101 Tyson Ave, Philadelphia, PA 19149, United States",
-        name: "Feast Of Justice",
-      },
-      {
-        id: 2,
-        // business_status: "OPERATIONAL",
-        formatted_address: "7965 Fillmore St, Philadelphia, PA 19111, United States",
-        name: "Food Distribution Center - Saint Timothy Luthern Church",
-      },
-      {
-        id: 3,
-        // business_status: "OPERATIONAL",
-        formatted_address: "510 Somerton Ave, Philadelphia, PA 19116, United States",
-        name: "Somerton Interfaith Food Bank",
-      }
-    ]
+    console.log(this.displayList)
+    this.displayList = true;
   }
 
 }
--- src/app/components/results-list/results-list.component.ts
+++ src/app/components/results-list/results-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchResult } from 'src/app/models/SearchResult';
+
 
 @Component({
   selector: 'app-results-list',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsListComponent implements OnInit {
 
+  results:SearchResult[];
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.results = [
+      {
+        id: 1,
+        // business_status: "OPERATIONAL",
+        formatted_address: "3101 Tyson Ave, Philadelphia, PA 19149, United States",
+        name: "Feast Of Justice",
+      },
+      {
+        id: 2,
+        // business_status: "OPERATIONAL",
+        formatted_address: "7965 Fillmore St, Philadelphia, PA 19111, United States",
+        name: "Food Distribution Center - Saint Timothy Luthern Church",
+      },
+      {
+        id: 3,
+        // business_status: "OPERATIONAL",
+        formatted_address: "510 Somerton Ave, Philadelphia, PA 19116, United States",
+        name: "Somerton Interfaith Food Bank",
+      }
+    ]
   }
 
 }
