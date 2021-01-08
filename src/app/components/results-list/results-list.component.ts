1a9f327746fa6440874924c392a902b7dfae3594
Aja Strommen <a.g.mechanix@gmail.com>
2021-01-08 01:14:04 -0600

    used foodbank service for comp data

--- src/app/components/results-list/results-list.component.ts
+++ src/app/components/results-list/results-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { SearchResult } from 'src/app/models/SearchResult';
+import { FoodBankService } from '../../services/FoodBank/food-bank.service'
 
 
 @Component({
@@ -8,32 +10,17 @@ import { SearchResult } from 'src/app/models/SearchResult';
   styleUrls: ['./results-list.component.scss']
 })
 export class ResultsListComponent implements OnInit {
-
+  foodbanks:any= { };
   results:SearchResult[];
   
-  constructor() { }
+  constructor(private foodbankService: FoodBankService) { }
 
-  ngOnInit(): void {
-    this.results = [
-      {
-        id: 1,
-        // business_status: "OPERATIONAL",
-        formatted_address: "3101 Tyson Ave, Philadelphia, PA 19149, United States",
-        name: "Feast Of Justice",
-      },
-      {
-        id: 2,
-        // business_status: "OPERATIONAL",
-        formatted_address: "7965 Fillmore St, Philadelphia, PA 19111, United States",
-        name: "Food Distribution Center - Saint Timothy Luthern Church",
-      },
-      {
-        id: 3,
-        // business_status: "OPERATIONAL",
-        formatted_address: "510 Somerton Ave, Philadelphia, PA 19116, United States",
-        name: "Somerton Interfaith Food Bank",
-      }
-    ]
+  ngOnInit() {
+    this.foodbankService.getFoodbanks().subscribe(data=>{
+      console.log(data);
+      this.foodbanks= data;
+      console.log(this.foodbanks.results[0]);
+      this.results = this.foodbanks.results;
+    });
   }
-
 }
