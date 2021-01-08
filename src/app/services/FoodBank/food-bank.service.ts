a48b5ee70c5d53b9072363cd641ad4f05438935a
Aja Strommen <a.g.mechanix@gmail.com>
2021-01-08 02:08:39 -0600

    add getZipcodeCoords func to foodbank service

--- src/app/services/FoodBank/food-bank.service.ts
+++ src/app/services/FoodBank/food-bank.service.ts
@@ -15,6 +15,8 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class FoodBankService {
+  openDataSoftAPIUrl: string = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=19111&facet=state&facet=timezone&facet=dst&refine.state=PA';
+
   resultsUrl:string = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=food+bank+19111&key=';
   // urlEnd:string = '&key='
 
@@ -26,4 +28,8 @@ export class FoodBankService {
   getFoodbanks(){
     return this.http.get(`${this.resultsUrl}${this.googleAPIKey}`)
   } 
+
+  getZipcodeCoords(){
+    return this.http.get(`${this.openDataSoftAPIUrl}`)
+  }
 }
