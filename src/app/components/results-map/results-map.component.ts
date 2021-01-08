148f7484ba41ea6165fd3893e99988a801ed7437
Aja Strommen <a.g.mechanix@gmail.com>
2021-01-08 02:09:33 -0600

    map shows using hardcoded 19111

--- src/app/components/results-map/results-map.component.ts
+++ src/app/components/results-map/results-map.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
+
+import { environment } from '../../../environments/environment';
+
+import { FoodBankService } from '../../services/FoodBank/food-bank.service'
 
 @Component({
   selector: 'app-results-map',
@@ -7,13 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsMapComponent implements OnInit {
 
+  map: google.maps.Map;
+
+  zipcodeData:any={ };
   display: boolean;
 
-  constructor() {
+  constructor(private foodbankService: FoodBankService) {
     this.display=true;
   }
 
   ngOnInit(): void {
+    this.foodbankService.getZipcodeCoords().subscribe(data=>{
+      console.log('zipcode data', data);
+      this.zipcodeData = data;
+      console.log(this.zipcodeData.records[0])
+      
+      const loader = new Loader({
+        apiKey: environment.googleKey
+      });
+      
+      loader.load().then(() => {
+        this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+          center: {lat: this.zipcodeData.records[0].fields.latitude, lng: this.zipcodeData.records[0].fields.longitude },
+          zoom: 15
+        });
+      })
+    })
   }
 
 }
